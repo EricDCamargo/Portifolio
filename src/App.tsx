fd2cf92d253d903e2b2875d95b7c248dc044b08f
Eric-Epiousion <eric.camargo@epiousion.com.br>
2023-08-10 08:05:47 -0300

    feat: Structuring project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <></>
+import styled from 'styled-components'
+import Home from './sections/home'
+import Header from './sections/header'
+import About from './sections/about'
+import Projects from './sections/projects'
+
+const App = () => {
+  return (
+    <PageContent>
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+    </PageContent>
+  )
 }
 
 export default App
+
+const PageContent = styled.div`
+  padding: 0;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+`
---
+++ src/shared/routes/index.tsx
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Routering
+} from 'react-router-dom'
+import App from '../../App'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Routering>
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<App />} />
+      </Routering>
+    </BrowserRouter>
+  )
+}
+
+export default Routes

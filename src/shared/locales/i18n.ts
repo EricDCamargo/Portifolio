b4d90d0c1773a34f08b98daa744f08852cfe92fa
EricDCamargo <ericdcamargo@gmail.com>
2023-08-18 11:36:08 -0300

    d

--- src/shared/locales/i18n.ts
+++ src/shared/locales/i18n.ts
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .init({
     supportedLngs: ['ptBR', 'en'],
-    fallbackLng: 'pt-BR',
+    fallbackLng: 'ptBR',
     resources: resources,
     detection: {
       order: ['path', 'localStorage', 'cookie', 'htmlTag', 'subdomain'],
